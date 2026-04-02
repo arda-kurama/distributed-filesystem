887ee2a1b69a8d635385e1626f5191b0444db8bc
Quinn Hynes <qwh405@icloud.com>
2026-04-01 21:48:26 -0400

    Improved terminal

--- Client.py
+++ Client.py
@@ -5,16 +5,18 @@ import json
 import http.client
 import time
 import hashlib
+import os
 
 from NameServer import NameServer
 
 BUFSIZE = 1024
 
 class Client:
-    def __init__(self, project_name, name_server):
+    def __init__(self, project_name, name_server, verbose):
         self.project_name = project_name
         self.name_server = name_server
         self.path = ''
+        self.verbose = verbose
 
         # keep a local list of files
         # what happens if another user adds file to directory?
@@ -24,102 +26,155 @@ class Client:
         pass
     
     def run_shell(self):
+        os.system('clear')
+        os.system('clear')
         while 1:
+            print('user@dist_filesys %', end=' ', flush=True)
             input = sys.stdin.readline().strip()
 
             args = input.split(' ')
             command = args[0]
-
-            # print(f'command: {command}')
-            # print(f'args: {args}')
+            
             self.handle_command(command, args)
     
     def handle_command(self, command, args):
-        # ls, cd, pwd, create, mkdir, open, remove
         match command:
             case 'ls':
                 if not self.current_directory_files:
                     # get list of files in current directory from name server
                     self.current_directory_files = self.name_server.ls(self.path)
-                print('Files: ', end='')
-                print(*self.current_directory_files)
+
+                if len(self.current_directory_files) != 0:
+                    print(*self.current_directory_files)
+
             case 'cd':
                 if len(args) == 1 and self.path != '': # go to root directory
-                    self.path = ''
-                    self.current_directory_files = None
-                    print(f'New path: {self.path}')
-                elif len(args) == 2:
-                    if args[1] == '..' and self.path != '': # move up directory
-                        self.path = self.path.rpartition('/')[0]
+                    if self.path != '':
                         self.current_directory_files = None
-                        print(f'New path: {self.path}')
-                    else:
-                        # make sure argument is valid name (no spaces, other weird characters)
-                        # to-do
-
-                        # check with name server if argument is a directory
-                        # to-do
-
-                        # return status, also list of files/directory in new directory?
-                        
-                        self.path = f'{self.path}/{args[1]}'
-                        self.current_directory_files = None
-                        print(f'New path: {self.path}')
-                else:
-                    print('Usage: cd [directory]')
-            case 'pwd':
-                    print(f'Path: {self.path}')
-            case 'create':
-                if len(args) == 2:
-                    # create file on name server
-                    if self.name_server.create(self.path, args[1]):
-                        print(f'Created file {args[1]}')
-                        if self.current_directory_files:
-                            self.current_directory_files.append(args[1])
-                    else:
-                        print('File already exists/failed to create')
-                else:
-                    print('Usage: create [file]')
-            case 'mkdir':
-                if len(args) == 2:
-                    # make sure argument is valid name (no spaces, other weird characters)
                     
-                    # create directory on name server
-                    if self.name_server.mkdir(self.path, args[1]):
-                        print(f'Created file {args[1]}')
-                        if self.current_directory_files:
-                            self.current_directory_files.append(args[1])
+                    self.path = ''
+                    if self.verbose:
+                        print(f'new path: {self.path}')
+                    return
+                
+                if len(args) != 2:
+                    print('usage: cd [directory]')
+                    return
+                
+                dest_dir = args[1]
+                if dest_dir == '..': # move up directory
+                    if self.path == '':
+                        return
+                    self.path = self.path.rpartition('/')[0]
+                    self.current_directory_files = None
+                    if self.verbose:
+                        print(f'new path: {self.path}')
+                else:
+                    if not self.valid_filename(dest_dir):
+                        print(f'directory {dest_dir} does not exist')
+                        return
+                    
+                    if self.name_server.cd(self.path, dest_dir):
+                        self.path = f'{self.path}/{dest_dir}'
+                        self.current_directory_files = None
+                        if self.verbose:
+                            print(f'new path: {self.path}')
                     else:
-                        print('Directory already exists/failed to create')
-                    pass
+                        print(f'directory {dest_dir} does not exist')
+                    
+            case 'pwd':
+                if self.path == '':
+                    print('/')
                 else:
-                    print('Usage: mkdir [directory]')
-            case 'open':
-                if len(args) == 2:
-                    # get storage server to connect to from name server
-                    pass
-                else:
-                    print('Usage: open [file]')
+                    print(self.path)
+            
+            case 'create':
+                if len(args) != 2:
+                    print('usage: create [file]')
+                    return
+                
+                # is this idempotent?
+                file = args[1]
+                self.name_server.create(self.path, file)
+                if self.verbose:
+                    print(f'created file {file}')
+                if self.current_directory_files:
+                    self.current_directory_files.append(file)
+            
             case 'remove':
-                if len(args) == 2:
-                    # remove entry from name server
-                    if self.name_server.remove(args[1]):
-                        if self.current_directory_files:
-                            self.current_directory_files.remove(args[1])
-                    else:
-                        print('File did not exist/failed to remove')
-                    pass
-                else:
-                    print('Usage: remove [file]')
+                if len(args) != 2:
+                    print('usage: remove [file]')
+                    return
+
+                filename = args[1]
+
+                self.name_server.remove(filename)
+                if self.verbose:
+                    print(f'removed file {filename}')
+                if self.current_directory_files:
+                    self.current_directory_files.remove(filename)
+                
+            case 'mkdir':
+                if len(args) != 2:
+                    print('usage: mkdir [directory]')
+                    return
+
+                dirname = args[1]
+
+                if not self.valid_filename(dirname):
+                    print(f'{dirname} not a valid directory name')
+                    return
+                
+                self.name_server.mkdir(self.path, dirname)
+                if self.verbose:
+                    print(f'created directory {dirname}')
+                if self.current_directory_files:
+                    self.current_directory_files.append(dirname)
+            
+            case 'mkdir':
+                if len(args) != 2:
+                    print('usage: rmdir [directory]')
+                    return
+
+                dirname = args[1]
+
+                if not self.valid_filename(dirname):
+                    print(f'{dirname} not a valid directory name')
+                    return
+                
+                self.name_server.rmdir(self.path, dirname)
+                if self.verbose:
+                    print(f'removed directory {dirname}')
+                if self.current_directory_files:
+                    self.current_directory_files.remove(dirname)
+
+            case 'open':
+                if len(args) != 2:
+                    print('usage: open [file]')
+                    return
+
+                # get storage server to connect to from name server
+                pass
+            
+            case 'clear':
+                os.system('clear')
+            
+            case _:
+                print('invalid command')
             
             # vim interface
             # - allows you to read/edit file
             # - can save changes, pass them along to storage servers
+    
+    def valid_filename(self, filename):
+        allowed = 'abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._-'
+        return all(char in allowed for char in filename)
+
 
 def main():
     project = 'filesys'
     n = NameServer(project)
-    c = Client(project, n)
+    c = Client(project, n, True)
     c.run_shell()
 
 if __name__ == '__main__':
--- NameServer.py
+++ NameServer.py
@@ -12,45 +12,76 @@ class NameServer():
     def __init__(self, project_name):
         self.project_name = project_name
         self.files = dict() # path -> FileData()
+        self.directories = set()
         self.storage_servers = [None]
+
+        self.playback()
+    
+    # to-do: checkpoint and log system
+    def playback(self):
+        self.data_dir = f'{self.project_name}_name_server/data'
+        self.checkpoint = f'{self.server_name}_name_server/table.ckpt'
+        self.log = f'{self.server_name}_name_server/table.txn'
+        self.log_entries = 0
+    
+    def compact(self):
+        pass
+    
+    def clean_orphans(self):
+        # check for files on disk that are no longer in self.files and self.directories
+        pass
     
     def ls(self, client_path):
-        directory_files = set()
         client_path = f'{client_path}/'
+        directory_files = set()
+
         for path in self.files.keys():
             if path.startswith(client_path):
                 directory_files.add(path.partition(client_path)[2].partition('/')[0])
+        
+        for path in self.directories:
+            if path.startswith(client_path):
+                directory_files.add(path.partition(client_path)[2].partition('/')[0])
+        
         return list(directory_files)
 
     def cd(self, client_path, dest_dir):
-        # to-do
-        pass
+        dest_path = f'{client_path}/{dest_dir}'
+        return dest_path in self.directories
 
     def create(self, client_path, filename):
-        # check if file already exists
         path = f'{client_path}/{filename}'
-        if path in self.files:
-            return False
-        
-        # create unique file_id
-        file_id = uuid.uuid4().hex
 
-        # assign storage server based on file_id
-        storage_server = hash(path) % len(self.storage_servers)
-        
-        # store metadata in hash table
-        self.files[path] = FileData(file_id, 0, 0, storage_server)
-
-        return True
-
-    def mkdir(self, client_path, dirname):
-        # to-do
-        pass
+        if path not in self.files and path not in self.directories:
+            file_id = uuid.uuid4().hex
+            storage_server = hash(path) % len(self.storage_servers)
+            self.files[path] = FileData(file_id, 0, 0, storage_server)
 
     def remove(self, client_path, filename):
         path = f'{client_path}/{filename}'
         if path in self.files:
-            dict().pop(path)
+            self.files.pop(path)
             # tell storage servers to remove
-            return True
-        return False
+            # to-do
+
+    def mkdir(self, client_path, dirname):
+        dirpath = f'{client_path}/{dirname}'
+
+        if dirpath not in self.files and dirpath not in self.directories:
+            self.directories.add(dirpath)
+    
+    def rmdir(self, client_path, dirname):
+        dirpath = f'{client_path}/{dirname}'
+
+        if dirpath in self.directories:
+            self.directories.remove(dirpath)
+
+            # remove all files in that directory
+            for path in self.files.keys():
+                if path.startswith(dirpath):
+                    self.files.pop(path)
+        
+            for path in self.directories:
+                if path.startswith(dirpath):
+                    self.directories.remove(path)
+    
